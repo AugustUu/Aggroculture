d109b381308a3c10bb45363b5dd6bba102edb421
ILove8288 <Ilove8288@student.pps.net>
2023-09-23 08:15:56 -0700

    Made sun cycle into code
    No more are we using an animation for the sun cycle, it is now calculated in the code with a easily editable variable. I also made it have its own scene for easy orginization

--- dayTime.cs
+++ dayTime.cs
@@ -1,13 +1,16 @@
-using Godot;
 using System;
-using System.ComponentModel;
-
+using System.Runtime.CompilerServices;
+using Godot;
+//IMPORANT: TO CHANGE THE TIME IN THE DAY CHANGE THE TIME_IN_DAY VARIABLE
 public partial class dayTime : Timer
 {
+	int TIME_IN_DAY = 288;
 	int minutes;
 	int hours = 0;
 
 	int days = 0;
+	Vector3 rotX = new Vector3(0,0,0);
+	
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -19,11 +22,19 @@ public partial class dayTime : Timer
 
 	private void  _on_timeout()
 	{
+
+		MeshInstance3D sun = GetNode<MeshInstance3D>("Dial");
 		minutes += 5;
 		recalculate_time();
+		//To be made smooth sun transitions I dont really know how tweeners work.
+		//Tween sunTween = GetTree().CreateTween();
+		//sunTween.TweenProperty(sun,"rotation",,0.6);
+		//sunTween.TweenProperty(sun,"Rotate",Vector3.One, 2.0f);
+		sun.RotateX((float)(2*Math.PI/TIME_IN_DAY));
+		GD.Print(sun.Rotation);
 	}
 
-
+	//Changes from raw int time into hours, mins, days.
 	public void recalculate_time(){
 		if(minutes >= 60){
 			minutes -= 60;
@@ -34,13 +45,10 @@ public partial class dayTime : Timer
 			hours = 0;
 			days += 1;
 		}
-
-
-
+		//setting labels to show coresponding data.
 		Label onScreenTime = GetNode<Label>("Time");
 		Label onScreenDays = GetNode<Label>("Days");
 		onScreenTime.Text = hours.ToString()+" : "+minutes.ToString();
-
 		onScreenDays.Text = "Day "+days;
 	}
 }
