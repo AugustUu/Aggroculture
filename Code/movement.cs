984879e94d6c998780dff5bcba526ecef5ed2de4
AugustUu <auusitalo8059@student.pps.net>
2023-09-23 13:29:54 -0700

    fixed Settings

--- Code/movement.cs
+++ Code/movement.cs
@@ -77,25 +77,20 @@ public partial class movement : CharacterBody3D
 		if(Input.IsActionPressed("ADS")){
 			Vector2 player_pos = camera.UnprojectPosition(Position);
 			Vector2 mouse_pos = GetViewport().GetMousePosition();
-			velocity.X /= 3;
-			velocity.Y = 0;
-			velocity.Z /= 3;
+
 			playerRotation = Mathf.Atan2(player_pos.Y - mouse_pos.Y, mouse_pos.X - player_pos.X) - Mathf.Pi / 2; // this shit is fucked dont change
 		}else{
 			if (direction != Vector3.Zero){
 				playerRotation = new Vector2(velocity.Z,velocity.X).Angle();
 			}
 		}
-		
 
 
 		Vector3 rotation = model.Rotation;
-
 		rotation.Y = (float)Mathf.LerpAngle(rotation.Y,playerRotation,0.2);
-
 		model.Rotation = rotation;
 
-		GD.Print(direction);
+		//GD.Print(direction);
 
 		Velocity = velocity;
 		MoveAndSlide();
