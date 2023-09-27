a801e9dd373e5d3dc1b3101c46103a462c113d2f
AugustUu <auusitalo8059@student.pps.net>
2023-09-26 20:00:07 -0700

    added new world gen

--- Code/worldGen.cs
+++ Code/worldGen.cs
@@ -1,24 +1,54 @@
 using Godot;
 using System;
 
-public partial class worldGen : GridMap
+public partial class worldGen : Node3D
 {
     public override void _Ready()
     {
 
         FastNoiseLite noise = new();
-        noise.NoiseType = FastNoiseLite.NoiseTypeEnum.Simplex;
-        noise.FractalGain = 0;
+        noise.FractalOctaves = 6;
+        noise.NoiseType = FastNoiseLite.NoiseTypeEnum.Perlin;
 
-        for (int x = -200; x < 200; x++)
-        {
-            for (int z = -200; z < 200; z++)
-            {
-                int y = (int)Mathf.Floor(noise.GetNoise2D(x, z) * 3);
-                SetCellItem(new Vector3I(x, y, z), 0);
-            }
+        MeshInstance3D test = GetNode<MeshInstance3D>("worldMesh");
+        PlaneMesh plane = (PlaneMesh)test.Mesh;
+        plane.Size = new Vector2(400,400);
+        plane.SubdivideDepth = 200;
+        plane.SubdivideWidth = 200;
+
+        SurfaceTool surface_tool = new SurfaceTool();
+        surface_tool.CreateFrom(plane,0);
+
+        ArrayMesh array_mesh = surface_tool.Commit();
+
+        MeshDataTool mesh_data_tool = new MeshDataTool();
+        mesh_data_tool.CreateFromSurface(array_mesh,0);
+
+
+        for(int i=0; i<mesh_data_tool.GetVertexCount(); i++){
+            Vector3 vertex = mesh_data_tool.GetVertex(i);
+            vertex.Y = noise.GetNoise3D(vertex.X,vertex.Y, vertex.Z)*60;
+            mesh_data_tool.SetVertex(i,vertex);
         }
-        //this.SetCellItem
+
+        array_mesh.ClearSurfaces();
+
+        mesh_data_tool.CommitToSurface(array_mesh);
+        surface_tool.Begin(Mesh.PrimitiveType.Triangles);
+        surface_tool.CreateFrom(array_mesh,0);
+        surface_tool.GenerateNormals();
+
+
+        //MeshInstance3D world = new MeshInstance3D();
+        test.Mesh = surface_tool.Commit();
+        
+
+
+        //AddChild(world);
+        test.CreateTrimeshCollision();
+        //GD.Print(world.GetChildren()[0].GetChildren());
+        
+
     }
 
     public override void _Process(double delta)
