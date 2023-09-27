6bbcf0d8c2e35238cd42e73850718b0aef07071f
tschmidt8320 <tschmidt8320@student.pps.net>
2023-09-26 21:35:18 -0700

    changed zooming
    zooming now changes position of camera relative to player instead of fov, has min and max but those are pretty arbitrary

--- Code/Zoom.cs
+++ Code/Zoom.cs
@@ -6,11 +6,16 @@ public partial class Zoom : Camera3D
 	//called when key pressed
 	public override void _UnhandledInput(InputEvent @event){
 		if(@event.IsActionPressed("zoom-in")){
-			this.Fov -= 5f;
+			this.Position = new Vector3(this.Position.X, this.Position.Y*0.8f, this.Position.Z*0.8f);
+			GD.Print(this.Position);
 		}
 		if(@event.IsActionPressed("zoom-out")){
-			this.Fov += 5f;
+			this.Position = new Vector3(this.Position.X, this.Position.Y*1.25f, this.Position.Z*1.25f);
+			GD.Print(this.Position);
 		}
-		this.Fov = Mathf.Clamp(this.Fov,40f,100f);
+		Vector3 clampPos = this.Position;
+		clampPos.Y = Mathf.Clamp(clampPos.Y, 2.1f, 12.5f);
+		clampPos.Z = Mathf.Clamp(clampPos.Z, 0.786f, 4.688f);
+		this.Position = clampPos;
 	}
 }
